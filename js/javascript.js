bdbd3c0641566e9a952e7cb684bd77af254c1ffd
paola <bernodiana@gmail.com>
2017-07-26 14:25:40 -0500

    añadir listas

---
+++ js/javascript.js
@@ -0,0 +1,81 @@
+
+var arrayDatos=[];  
+function mastareas() {
+  var elemento = document.getElementById('elemento').value;
+      arrayDatos.push(elemento);
+               document.getElementById('elemento').value="";
+  var resultado = '';
+  for(i in arrayDatos){
+    resultado += arrayDatos[i] +'<br>';
+  }
+  document.getElementById('resultado').innerHTML = resultado; 
+
+}
+
+var array =[ {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  },]
+ var html = "";
+ for (var i =0; i<array.length;i++){
+  var dato = array[i];
+  var s = "<li>" +  dato.title + "</li>";
+  html += s;
+  }
+document.getElementById("listas").innerHTML+=html;
